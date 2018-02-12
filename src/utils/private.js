c7277dbac480e7b994a8d99354808dc9a5786f37
Copple <pcopplestone@gmail.com>
2018-02-12 18:40:49 +0800

    linting code

--- src/utils/private.js
+++ src/utils/private.js
@@ -1,6 +1,6 @@
 var exports = module.exports = {}
 
 // Logs the message. Override `this.logger` for specialized logging. noops by default
-exports.log = function(logger, msg, data){
+exports.log = function (logger, msg, data) {
   logger(msg, data)
 }
